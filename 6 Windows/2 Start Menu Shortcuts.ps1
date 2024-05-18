2a3a2d078ccef6b9db56f5aeff70b110b80e942a
FR33THY <168888348+fr33thytweaks@users.noreply.github.com>
2024-05-18 23:00:05 +1000

    Update 2 Start Menu Shortcuts.ps1

--- 6 Windows/2 Start Menu Shortcuts.ps1
+++ 6 Windows/2 Start Menu Shortcuts.ps1
@@ -7,17 +7,27 @@
     $Host.PrivateData.ProgressForegroundColor = "White"
     Clear-Host
 
-# create start menu shortcuts 1 in start menu
+# create startmenu shortcuts 1 in start menu
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Start Menu Shortcuts 1.lnk")
+$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartMenu Shortcuts 1.lnk")
 $Shortcut.TargetPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs"
 $Shortcut.Save()
-# create start menu shortcuts 2 in start menu
+# create startmenu shortcuts 2 in start menu
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Start Menu Shortcuts 2.lnk")
+$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartMenu Shortcuts 2.lnk")
 $Shortcut.TargetPath = "$env:AppData\Microsoft\Windows\Start Menu\Programs"
 $Shortcut.Save()
-# open start menu shortcuts 1 location
+# create startup programs 1 in start menu
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp Programs 1.lnk")
+$Shortcut.TargetPath = "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup"
+$Shortcut.Save()
+# create startup programs 2 in start menu
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp Programs 2.lnk")
+$Shortcut.TargetPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+$Shortcut.Save()
+# open startmenu shortcuts 1 location
 Start-Process "$env:ProgramData\Microsoft\Windows\Start Menu\Programs"
-# open start menu shortcuts 2 location
+# open startmenu shortcuts 2 location
 Start-Process "$env:AppData\Microsoft\Windows\Start Menu\Programs"
