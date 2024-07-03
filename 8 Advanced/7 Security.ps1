d2c56c7132f5ce8cf32fb220276b8ffe9e0a9e4a
FR33THY <168888348+fr33thytweaks@users.noreply.github.com>
2024-07-04 00:51:21 +1000

    Update 7 Security.ps1
    removed dependency for powerun

--- 8 Advanced/7 Security.ps1
+++ 8 Advanced/7 Security.ps1
@@ -6,39 +6,37 @@
 	$Host.PrivateData.ProgressBackgroundColor = "Black"
     $Host.PrivateData.ProgressForegroundColor = "White"
     Clear-Host
-
-    function Get-FileFromWeb {
-    param ([Parameter(Mandatory)][string]$URL, [Parameter(Mandatory)][string]$File)
-    function Show-Progress {
-    param ([Parameter(Mandatory)][Single]$TotalValue, [Parameter(Mandatory)][Single]$CurrentValue, [Parameter(Mandatory)][string]$ProgressText, [Parameter()][int]$BarSize = 10, [Parameter()][switch]$Complete)
-    $percent = $CurrentValue / $TotalValue
-    $percentComplete = $percent * 100
-    if ($psISE) { Write-Progress "$ProgressText" -id 0 -percentComplete $percentComplete }
-    else { Write-Host -NoNewLine "`r$ProgressText $(''.PadRight($BarSize * $percent, [char]9608).PadRight($BarSize, [char]9617)) $($percentComplete.ToString('##0.00').PadLeft(6)) % " }
-    }
-    try {
-    $request = [System.Net.HttpWebRequest]::Create($URL)
-    $response = $request.GetResponse()
-    if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) { throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'." }
-    if ($File -match '^\.\\') { $File = Join-Path (Get-Location -PSProvider 'FileSystem') ($File -Split '^\.')[1] }
-    if ($File -and !(Split-Path $File)) { $File = Join-Path (Get-Location -PSProvider 'FileSystem') $File }
-    if ($File) { $fileDirectory = $([System.IO.Path]::GetDirectoryName($File)); if (!(Test-Path($fileDirectory))) { [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null } }
-    [long]$fullSize = $response.ContentLength
-    [byte[]]$buffer = new-object byte[] 1048576
-    [long]$total = [long]$count = 0
-    $reader = $response.GetResponseStream()
-    $writer = new-object System.IO.FileStream $File, 'Create'
-    do {
-    $count = $reader.Read($buffer, 0, $buffer.Length)
-    $writer.Write($buffer, 0, $count)
-    $total += $count
-    if ($fullSize -gt 0) { Show-Progress -TotalValue $fullSize -CurrentValue $total -ProgressText " $($File.Name)" }
-    } while ($count -gt 0)
-    }
-    finally {
-    $reader.Close()
-    $writer.Close()
-    }
+	
+    function RunAsTI($cmd, $arg) {
+    $id = 'RunAsTI'; $key = "Registry::HKU\$(((whoami /user)-split' ')[-1])\Volatile Environment"; $code = @'
+    $I=[int32]; $M=$I.module.gettype("System.Runtime.Interop`Services.Mar`shal"); $P=$I.module.gettype("System.Int`Ptr"); $S=[string]
+    $D=@(); $T=@(); $DM=[AppDomain]::CurrentDomain."DefineDynami`cAssembly"(1,1)."DefineDynami`cModule"(1); $Z=[uintptr]::size
+    0..5|% {$D += $DM."Defin`eType"("AveYo_$_",1179913,[ValueType])}; $D += [uintptr]; 4..6|% {$D += $D[$_]."MakeByR`efType"()}
+    $F='kernel','advapi','advapi', ($S,$S,$I,$I,$I,$I,$I,$S,$D[7],$D[8]), ([uintptr],$S,$I,$I,$D[9]),([uintptr],$S,$I,$I,[byte[]],$I)
+    0..2|% {$9=$D[0]."DefinePInvok`eMethod"(('CreateProcess','RegOpenKeyEx','RegSetValueEx')[$_],$F[$_]+'32',8214,1,$S,$F[$_+3],1,4)}
+    $DF=($P,$I,$P),($I,$I,$I,$I,$P,$D[1]),($I,$S,$S,$S,$I,$I,$I,$I,$I,$I,$I,$I,[int16],[int16],$P,$P,$P,$P),($D[3],$P),($P,$P,$I,$I)
+    1..5|% {$k=$_; $n=1; $DF[$_-1]|% {$9=$D[$k]."Defin`eField"('f' + $n++, $_, 6)}}; 0..5|% {$T += $D[$_]."Creat`eType"()}
+    0..5|% {nv "A$_" ([Activator]::CreateInstance($T[$_])) -fo}; function F ($1,$2) {$T[0]."G`etMethod"($1).invoke(0,$2)}
+    $TI=(whoami /groups)-like'*1-16-16384*'; $As=0; if(!$cmd) {$cmd='control';$arg='admintools'}; if ($cmd-eq'This PC'){$cmd='file:'}
+    if (!$TI) {'TrustedInstaller','lsass','winlogon'|% {if (!$As) {$9=sc.exe start $_; $As=@(get-process -name $_ -ea 0|% {$_})[0]}}
+    function M ($1,$2,$3) {$M."G`etMethod"($1,[type[]]$2).invoke(0,$3)}; $H=@(); $Z,(4*$Z+16)|% {$H += M "AllocHG`lobal" $I $_}
+    M "WriteInt`Ptr" ($P,$P) ($H[0],$As.Handle); $A1.f1=131072; $A1.f2=$Z; $A1.f3=$H[0]; $A2.f1=1; $A2.f2=1; $A2.f3=1; $A2.f4=1
+    $A2.f6=$A1; $A3.f1=10*$Z+32; $A4.f1=$A3; $A4.f2=$H[1]; M "StructureTo`Ptr" ($D[2],$P,[boolean]) (($A2 -as $D[2]),$A4.f2,$false)
+    $Run=@($null, "powershell -win 1 -nop -c iex `$env:R; # $id", 0, 0, 0, 0x0E080600, 0, $null, ($A4 -as $T[4]), ($A5 -as $T[5]))
+    F 'CreateProcess' $Run; return}; $env:R=''; rp $key $id -force; $priv=[diagnostics.process]."GetM`ember"('SetPrivilege',42)[0]
+    'SeSecurityPrivilege','SeTakeOwnershipPrivilege','SeBackupPrivilege','SeRestorePrivilege' |% {$priv.Invoke($null, @("$_",2))}
+    $HKU=[uintptr][uint32]2147483651; $NT='S-1-5-18'; $reg=($HKU,$NT,8,2,($HKU -as $D[9])); F 'RegOpenKeyEx' $reg; $LNK=$reg[4]
+    function L ($1,$2,$3) {sp 'HKLM:\Software\Classes\AppID\{CDCBCFCA-3CDC-436f-A4E2-0E02075250C2}' 'RunAs' $3 -force -ea 0
+    $b=[Text.Encoding]::Unicode.GetBytes("\Registry\User\$1"); F 'RegSetValueEx' @($2,'SymbolicLinkValue',0,6,[byte[]]$b,$b.Length)}
+    function Q {[int](gwmi win32_process -filter 'name="explorer.exe"'|?{$_.getownersid().sid-eq$NT}|select -last 1).ProcessId}
+    $11bug=($((gwmi Win32_OperatingSystem).BuildNumber)-eq'22000')-AND(($cmd-eq'file:')-OR(test-path -lit $cmd -PathType Container))
+    if ($11bug) {'System.Windows.Forms','Microsoft.VisualBasic' |% {[Reflection.Assembly]::LoadWithPartialName("'$_")}}
+    if ($11bug) {$path='^(l)'+$($cmd -replace '([\+\^\%\~\(\)\[\]])','{$1}')+'{ENTER}'; $cmd='control.exe'; $arg='admintools'}
+    L ($key-split'\\')[1] $LNK ''; $R=[diagnostics.process]::start($cmd,$arg); if ($R) {$R.PriorityClass='High'; $R.WaitForExit()}
+    if ($11bug) {$w=0; do {if($w-gt40){break}; sleep -mi 250;$w++} until (Q); [Microsoft.VisualBasic.Interaction]::AppActivate($(Q))}
+    if ($11bug) {[Windows.Forms.SendKeys]::SendWait($path)}; do {sleep 7} while(Q); L '.Default' $LNK 'Interactive User'
+'@; $V = ''; 'cmd', 'arg', 'id', 'key' | ForEach-Object { $V += "`n`$$_='$($(Get-Variable $_ -val)-replace"'","''")';" }; Set-ItemProperty $key $id $($V, $code) -type 7 -force -ea 0
+    Start-Process powershell -args "-win 1 -nop -c `n$V `$env:R=(gi `$key -ea 0).getvalue(`$id)-join''; iex `$env:R" -verb runas -Wait
     }
 
     Write-Host "1. Security: Off"
@@ -78,17 +76,15 @@ Write-Host ""
 Pause
 Clear-Host
 Write-Host "Step: One. Please wait . . ."
-# download powerrun
-Get-FileFromWeb -URL "https://www.sordum.org/files/downloads.php?power-run" -File "$env:TEMP\PowerRun.zip"
-# extract files
-Expand-Archive "$env:TEMP\PowerRun.zip" -DestinationPath "$env:TEMP" -ErrorAction SilentlyContinue
 # disable exploit protection, leaving control flow guard cfg on for vanguard anticheat
 cmd /c "reg add `"HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel`" /v `"MitigationOptions`" /t REG_BINARY /d `"222222000001000000000000000000000000000000000000`" /f >nul 2>&1"
-Timeout /T 1 | Out-Null
-# disable exploit protection with powerrun, leaving control flow guard cfg on for vanguard anticheat
-Set-Location -Path "$env:TEMP\PowerRun"
-.\PowerRun_x64.exe /SW:0 cmd.exe /c reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel" /v "MitigationOptions" /t REG_BINARY /d "222222000001000000000000000000000000000000000000" /f
-Timeout /T 1 | Out-Null
+Timeout /T 2 | Out-Null
+# disable exploit protection with RunAsTI, leaving control flow guard cfg on for vanguard anticheat
+$ExploitProtection = @'
+cmd /c "reg add `"HKLM\SYSTEM\ControlSet001\Control\Session Manager\kernel`" /v `"MitigationOptions`" /t REG_BINARY /d `"222222000001000000000000000000000000000000000000`" /f >nul 2>&1"
+'@
+RunAsTI powershell "-nologo -windowstyle hidden -command $ExploitProtection"
+Timeout /T 2 | Out-Null
 # create reg file
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
@@ -502,7 +498,7 @@ Windows Registry Editor Version 5.00
 [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell]
 "ExecutionPolicy"="Unrestricted"
 "@
-Set-Content -Path "$env:TEMP\PowerRun\Security Off.reg" -Value $MultilineComment -Force
+Set-Content -Path "$env:TEMP\SecurityOff.reg" -Value $MultilineComment -Force
 # disable scheduled tasks
 schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Disable | Out-Null
 schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Disable | Out-Null
@@ -543,17 +539,21 @@ Pause
 Clear-Host
 Write-Host "Step: Two. Please wait . . ."
 # import reg file
-Regedit.exe /S "$env:TEMP\PowerRun\Security Off.reg"
+Regedit.exe /S "$env:TEMP\SecurityOff.reg"
 Timeout /T 5 | Out-Null
-# import reg file powerrun
-Set-Location -Path "$env:TEMP\PowerRun"
-.\PowerRun_x64.exe Regedit.exe /S "Security Off.reg"
+# import reg file RunAsTI
+$SecurityOff = @'
+Regedit.exe /S "$env:TEMP\SecurityOff.reg"
+'@
+RunAsTI powershell "-nologo -windowstyle hidden -command $SecurityOff"
 Timeout /T 5 | Out-Null
 # stop smartscreen running
 Stop-Process -Force -Name smartscreen -ErrorAction SilentlyContinue | Out-Null
 # move smartscreen
-Set-Location -Path "$env:TEMP\PowerRun"
-.\PowerRun_x64.exe /SW:0 cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\smartscreen.exe"
+$SmartScreen = @'
+cmd.exe /c move /y "C:\Windows\System32\smartscreen.exe" "C:\Windows\smartscreen.exe"
+'@
+RunAsTI powershell "-nologo -windowstyle hidden -command $SmartScreen"
 Timeout /T 5 | Out-Null
 # disable windows-defender-default-definitions
 Dism /Online /NoRestart /Disable-Feature /FeatureName:Windows-Defender-Default-Definitions | Out-Null
@@ -586,10 +586,6 @@ exit
 
 Clear-Host
 Write-Host "Step: One. Please wait . . ."
-# download powerrun
-Get-FileFromWeb -URL "https://www.sordum.org/files/downloads.php?power-run" -File "$env:TEMP\PowerRun.zip"
-# extract files
-Expand-Archive "$env:TEMP\PowerRun.zip" -DestinationPath "$env:TEMP" -ErrorAction SilentlyContinue
 # create reg file
 $MultilineComment = @"
 Windows Registry Editor Version 5.00
@@ -1004,7 +1000,7 @@ Windows Registry Editor Version 5.00
 [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell]
 "ExecutionPolicy"="Restricted"
 "@
-Set-Content -Path "$env:TEMP\PowerRun\Security On.reg" -Value $MultilineComment -Force
+Set-Content -Path "$env:TEMP\SecurityOn.reg" -Value $MultilineComment -Force
 # enable scheduled tasks
 schtasks /Change /TN "Microsoft\Windows\ExploitGuard\ExploitGuard MDM policy Refresh" /Enable | Out-Null
 schtasks /Change /TN "Microsoft\Windows\Windows Defender\Windows Defender Cache Maintenance" /Enable | Out-Null
@@ -1026,15 +1022,19 @@ exit
 Clear-Host
 Write-Host "Step: Two. Please wait . . ."
 # import reg file
-Regedit.exe /S "$env:TEMP\PowerRun\Security On.reg"
+Regedit.exe /S "$env:TEMP\SecurityOn.reg"
 Timeout /T 5 | Out-Null
-# import reg file powerrun
-Set-Location -Path "$env:TEMP\PowerRun"
-.\PowerRun_x64.exe Regedit.exe /S "Security On.reg"
+# import reg file RunAsTI
+$SecurityOn = @'
+Regedit.exe /S "$env:TEMP\SecurityOn.reg"
+'@
+RunAsTI powershell "-nologo -windowstyle hidden -command $SecurityOn"
 Timeout /T 5 | Out-Null
 # move smartscreen
-Set-Location -Path "$env:TEMP\PowerRun"
-.\PowerRun_x64.exe /SW:0 cmd.exe /c move /y "C:\Windows\smartscreen.exe" "C:\Windows\System32\smartscreen.exe"
+$SmartScreen = @'
+cmd.exe /c move /y "C:\Windows\smartscreen.exe" "C:\Windows\System32\smartscreen.exe"
+'@
+RunAsTI powershell "-nologo -windowstyle hidden -command $SmartScreen"
 Timeout /T 5 | Out-Null
 # enable windows-defender-default-definitions (can't turn back on)
 # Dism /Online /NoRestart /Enable-Feature /FeatureName:Windows-Defender-Default-Definitions | Out-Null
