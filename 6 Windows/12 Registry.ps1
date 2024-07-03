f6c1bc179284d9397af2b3bc15de64c31e5b3cd1
FR33THY <168888348+fr33thytweaks@users.noreply.github.com>
2024-07-03 13:47:47 +1000

    Update 12 Registry.ps1
    fix typo

--- 6 Windows/12 Registry.ps1
+++ 6 Windows/12 Registry.ps1
@@ -100,7 +100,7 @@ Windows Registry Editor Version 5.00
 [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced]
 "HideFileExt"=dword:00000000
 
-; disable search histroy
+; disable search history
 [HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings]
 "IsDeviceSearchHistoryEnabled"=dword:00000000
 
@@ -1253,7 +1253,7 @@ Windows Registry Editor Version 5.00
 [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced]
 "HideFileExt"=dword:00000001
 
-; search histroy
+; search history
 [HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings]
 "IsDeviceSearchHistoryEnabled"=-
 
