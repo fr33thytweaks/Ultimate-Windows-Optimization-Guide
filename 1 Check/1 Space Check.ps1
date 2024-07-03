3e83cfa1e52347a4c7c8e03d5de7dc7d7a8fd742
FR33THY <168888348+fr33thytweaks@users.noreply.github.com>
2024-07-03 17:07:57 +1000

    Update 1 Space Check.ps1
    added % of current windows drive

--- 1 Check/1 Space Check.ps1
+++ 1 Check/1 Space Check.ps1
@@ -7,6 +7,10 @@
     $Host.PrivateData.ProgressForegroundColor = "White"
     Clear-Host
 
-Start-Process explorer shell:MyComputerFolder
-Write-Host "Maintain at least 10% free space."
+Write-Host "Maintain at least 10% free space"
+$driveletter = $env:SystemDrive -replace ':', ''
+$volume = Get-Volume $driveletter | Select-Object Size,SizeRemaining
+$percentRemain = ($volume.SizeRemaining / $volume.Size) * 100
+Write-Host "Free space =" "$($percentRemain.ToString().substring(0,4))%"
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Start-Process explorer shell:MyComputerFolder
